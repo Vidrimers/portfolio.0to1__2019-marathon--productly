7e887b7fd69388e36e34dc840a93bf26e4294609
Jaroslav <vidrimers2@gmail.com>
2021-03-20 14:10:56 +0300

    final

--- js/main.js
+++ js/main.js
@@ -6,6 +6,24 @@ $(function () {
     slidesToScroll: 3,
     arrows: false,
     dots: true,
+    responsive: [{
+      breakpoint: 769,
+      settings:{
+        slidesToShow: 2,
+    slidesToScroll: 2,
+
+      }
+    },{
+      breakpoint: 577,
+      settings:{
+        slidesToShow: 1,
+    slidesToScroll: 1,
+
+      }
+    }]
   });
+  $('.menu__btn').on('click', function(){
+    $('.menu__list').toggleClass('menu__list--active')
+  })
 
 });
